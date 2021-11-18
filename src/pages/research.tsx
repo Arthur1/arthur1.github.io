9ccd500bf5a6dbee6e03f9c5db7ea25630b54435
Kazuki Asakura <32762324+Arthur1@users.noreply.github.com>
2021-11-19 04:54:10 +0900

    Research Page

---
+++ src/pages/research.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import type { FC } from 'react'
+import { graphql } from 'gatsby'
+import { useTranslation } from 'gatsby-plugin-react-i18next'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
+import Container from '../components/Container'
+import Headline1 from '../components/Headline1'
+import Headline2 from '../components/Headline2'
+import * as styles from './research.module.scss'
+
+const ResearchPage: FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Seo title="Research" />
+      <Layout>
+        <Headline1 backgroundUrl="/images/headline-bg/research.png">Research</Headline1>
+        <Container>
+          <Headline2>{t('interests.title')}</Headline2>
+          <ul>
+            <li>{t('interests.interests.generalizedSampling')}</li>
+            <li>{t('interests.interests.nlp')}</li>
+            <li>{t('interests.interests.boardGameAi')}</li>
+            <li>{t('interests.interests.postWeb')}</li>
+          </ul>
+          <Headline2>{t('publications.title')}</Headline2>
+          <ol className={styles.publicationsList}>
+            <li>{t('publications.publications.vric2020')}</li>
+            <li>{t('publications.publications.vrsj2019')}</li>
+          </ol>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default ResearchPage
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`
