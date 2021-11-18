92518f509e181e0153b03d51a0fa5fc853ec5dbc
Kazuki Asakura <32762324+Arthur1@users.noreply.github.com>
2021-11-19 04:28:20 +0900

    add globe icon to LangSwicthersList

--- src/components/Navigation/LangSwitchersList.tsx
+++ src/components/Navigation/LangSwitchersList.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import type { FC } from 'react'
+import { FaGlobe } from 'react-icons/fa'
 import LangSwitcher from './LangSwitcher'
 import * as styles from './LangSwitchersList.module.scss'
 
 const LangSwitcherList: FC = () => (
   <div className={styles.langSwitcherList}>
-    <LangSwitcher lang="ja">日本語</LangSwitcher> | <LangSwitcher lang="en">English</LangSwitcher>
+    <FaGlobe /> <LangSwitcher lang="ja">日本語</LangSwitcher> |{' '}
+    <LangSwitcher lang="en">English</LangSwitcher>
   </div>
 )
 
